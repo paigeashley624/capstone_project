5749e93e8452c5695a84d155fc39836c23541cdb
Paige Morton <paigeashley624@gmail.com>
2021-05-17 16:09:17 -0400

    add netlify link to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "example.com"
+    origins "lbook-stash.netlify.app", "example.com"
     # update to front-end netlify link when setup
     resource "*",
       headers: :any,
